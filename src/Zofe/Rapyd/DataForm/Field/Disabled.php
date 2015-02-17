2acced9bd3a31a02712d3d8375ecd4dd44f92c59
Nimdis <tsaazi@gmail.com>
2015-02-17 12:57:45 +0600

    added disabled field;

---
+++ src/Zofe/Rapyd/DataForm/Field/Disabled.php
@@ -0,0 +1,44 @@
+<?php namespace Zofe\Rapyd\DataForm\Field;
+
+use Illuminate\Support\Facades\Form;
+
+class Disabled extends Field
+{
+  public $type = "disabled";
+
+  public function build()
+  {
+    $output = "";
+
+    if (parent::build() === false) return;
+
+    switch ($this->status) {
+      case "disabled":
+      case "show":
+
+        if ($this->type =='hidden' || $this->value == "") {
+          $output = "";
+        } elseif ( (!isset($this->value)) ) {
+          $output = $this->layout['null_label'];
+        } else {
+          $output = nl2br(htmlspecialchars($this->value));
+        }
+        $output = "<div class='help-block'>".$output."&nbsp;</div>";
+        break;
+
+      case "create":
+      case "modify":
+        $this->attributes['disabled'] = true;
+        $output = Form::text($this->name, $this->value, $this->attributes);
+        break;
+
+      case "hidden":
+        $output = Form::hidden($this->name, $this->value);
+        break;
+
+      default:;
+    }
+    $this->output = "\n".$output."\n". $this->extra_output."\n";
+  }
+
+}
